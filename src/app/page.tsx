aaeccfe369144a170f10982e9875d2167362ff87
Kauã Berman Amorim <kauabamorim@outlook.com>
2025-07-23 21:21:08 -0300

    Feat: start project next with shadcn ui

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  );
+}
